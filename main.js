18e66b8c68b078e9b9f34c07c009f780ab315f8c
Martin Robinson <martinjrobins@gmail.com>
2019-06-21 07:20:48 +0100

    add custom menu

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
-const { app, BrowserWindow } = require('electron');
+const {
+  app,
+  BrowserWindow,
+  Menu
+} = require('electron');
 
 let win = null;
 
+const menu_template = [{
+  label: 'File',
+  submenu: [{
+      label: 'Open',
+      click: () => {
+        console.log('Open Clicked');
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]
+}];
+
+
 createWindow = () => {
-    win = new BrowserWindow({
-	width:800,
-	height:600,
-	webPreferences: {
-	    nodeIntegration: true,
-	},
-    });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    win.loadFile('index.html');
+  win.loadFile('index.html');
 
-    win.on('closed', () => {
-	win = null;
-    });
+  win.on('closed', () => {
+    win = null;
+  });
 }
 
-app.on('ready', createWindow);
+createMenu = () => {
+  const menu = Menu.buildFromTemplate(menu_template);
+  Menu.setApplicationMenu(menu);
+}
+
+
+app.on('ready', function() {
+  createWindow();
+  createMenu();
+});
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-	app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (win === null) {
-	createWindow();
-    }
+  if (win === null) {
+    createWindow();
+  }
 });
