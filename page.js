3d3944fe6d178c48a51a0e25544093affb6ce405
Martin Robinson <martinjrobins@gmail.com>
2019-07-07 06:57:56 +0100

    #1 output arrays back to javascript

--- page.js
+++ page.js
@@ -13,25 +13,25 @@ viewer = OpenSeadragon({
   zoomPerScroll: 2
 });
 
-viewer.addHandler("update-tile", function (data) {
+viewer.addHandler("update-tile", function(data) {
   console.log("update-tile")
 });
 
 function getImageData(img) {
-    // Create an empty canvas element
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+  // Create an empty canvas element
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
 
-    // Copy the image contents to the canvas
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
+  // Copy the image contents to the canvas
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
 
-    // Return all the pixel data
-    return ctx.getImageData(0, 0, canvas.width, canvas.height)
+  // Return all the pixel data
+  return ctx.getImageData(0, 0, canvas.width, canvas.height)
 }
 
-viewer.addHandler("tile-loaded", function (data) {
+viewer.addHandler("tile-loaded", function(data) {
   console.log("tile-loaded")
   console.log(`\tlevel=${data.tile.level}`)
   console.log(`\tbounds=${data.tile.bounds}`)
@@ -41,11 +41,16 @@ viewer.addHandler("tile-loaded", function (data) {
   console.log(`\timage type=${typeof data.image}`)
   var img_data = getImageData(data.image)
   console.log(`\tdata type=${typeof data}`)
-  slic.slic(img_data.data, img_data.width, img_data.height)
-  console.log(`data[0] = ${img_data.data[0]}`)
+
+  const [
+    outlabels, outLABMeanintensities,
+    outPixelCounts, outseedsXY,
+    outLABVariances, outCollectedFeatures
+  ] = slic.slic(img_data.data, img_data.width, img_data.height)
+
 });
 
-viewer.addHandler("tile-unloaded", function (data) {
+viewer.addHandler("tile-unloaded", function(data) {
   console.log("tile-unloaded")
 });
 
