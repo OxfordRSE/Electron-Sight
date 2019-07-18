9c7de4084059f6f66eb0a3a6a103cc0d88633f77
Martin Robinson <martinjrobins@gmail.com>
2019-07-18 13:33:59 +0100

    #9 add basic file viewer in left pane

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 const electron = window.require('electron');
+import {Treebeard} from 'react-treebeard';
 const remote = electron.remote
 const fs = remote.require('fs');
 
+
+class FileTree extends React.Component {
+    constructor(props){
+        super(props);
+        var path = props.path;
+        this.state = {data: FileTree.readDir(path)};
+    }
+
+    static readDir(path) {
+        var data = {
+          name: 'Click on a file to open',
+          toggled: true,
+          children: []
+        };
+
+        fs.readdirSync(path).forEach(file => {
+            var fileInfo = {
+              name: file,
+              path: `${path}/${file}`,
+              children: []
+            };
+
+            var stat = fs.statSync(fileInfo.path);
+
+            if (stat.isDirectory()){
+                //fileInfo.items = FileTree.readDir(fileInfo.path);
+            }
+
+            data.children.push(fileInfo)
+        });
+
+        return data;
+    }
+
+    onToggle(node, toggled){
+        viewer.open('file://' + node.path)
+    }
+
+    render() {
+        const {data} = this.state;
+        return (
+            <Treebeard
+                data={data}
+                onToggle={this.onToggle}
+            />
+        );
+    }
+}
 
 
 class Viewer extends React.Component {
@@ -21,16 +70,31 @@ class Viewer extends React.Component {
       width: size.width,
       height: size.height
     };
-    return (<div className="viewer" id="Viewer" style={style}>
+    return (<div id="Viewer" style={style}>
     </div>);
+  }
+}
+
+
+class Panels extends React.Component {
+  render() {
+    const directory = fs.realpathSync('.');
+    //const directory = '.';
+
+    return (
+        <div id="LeftPane">
+          <FileTree path={directory} />
+      </div>
+    );
   }
 }
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="app" id="App">
-        <Viewer />
+      <div>
+      <Viewer />
+      <Panels />
       </div>
     );
   }
