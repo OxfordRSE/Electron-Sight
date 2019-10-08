ae754361bd332455d72427247977b9a4aff83ea7
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2019-10-08 16:08:02 +0100

    pass classifier prop to Predict

--- src/app.jsx
+++ src/app.jsx
@@ -38,6 +38,7 @@ export default class App extends React.Component {
         }}
       />
       <Predict
+        classifier={this.classifier}
         ref={predict => {
           this.predict = predict;
         }}
