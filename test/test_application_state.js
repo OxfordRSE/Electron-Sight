c4331c35b082e42e8af9de06cd23299d167d3f93
Graham Lee <graham.lee@cs.ox.ac.uk>
2019-12-12 17:36:33 +0000

    Test the menu state in View mode.

--- test/test_application_state.js
+++ test/test_application_state.js
@@ -46,4 +46,23 @@ describe('DisabledMode', function() {
         chai.assert(openSpy.calledWith('file:///tmp/filename.dzi'), 'Opened the file using OSD');
         chai.assert(nextMode.modeName === 'View', 'Transition to view mode');
     });
+});
+
+describe('ViewMode', function() {
+    const menu = { props: { openseadragon: { open: () => {} }}};
+    const nodeData = { path: '/tmp/filename.dzi' };
+    const viewMode = DefaultMode().openFile(menu, nodeData);
+    it('enables all of the buttons', function() {
+        chai.assert(viewMode.annotateButtonDisabled() === false);
+        chai.assert(viewMode.brightnessButtonDisabled() === false);
+        chai.assert(viewMode.contrastButtonDisabled() === false);
+        chai.assert(viewMode.classifierButtonDisabled() === false);
+        chai.assert(viewMode.predictButtonDisabled() === false);
+    });
+
+    it('activates none of the buttons', function() {
+        chai.assert(viewMode.annotateButtonActive() === false);
+        chai.assert(viewMode.classifierButtonActive() === false);
+        chai.assert(viewMode.predictButtonActive() === false);
+    })
 })
