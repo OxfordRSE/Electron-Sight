422190169a046ee2f564311fa099c23b94114cd0
Martin Robinson <martinjrobins@gmail.com>
2020-03-12 11:57:19 +0000

    #32 make attempt at a plot

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -3,7 +3,7 @@ import {
   H5,
   Divider,
 } from "@blueprintjs/core";
-import { VictoryBar } from 'victory';
+import { VictoryBar, VictoryChart, VictoryTheme } from 'victory';
 import WindowPortal from './WindowPortal';
 
 class Analytics extends React.Component {
@@ -13,12 +13,27 @@ class Analytics extends React.Component {
   }
 
   render() {
+    //Numbers of cells in each region (e.g., in different regions as boxplot)
+    //Cell densities (e.g., in different regions as boxplot)
+    //Distributions of distance between NN cells
+    //Distributions of distance between randomly chosen point and nearest cell (Spherical Contact Distance - SCD)
+    //Differences between these things in different annotated regions (e.g., annotating 2 distinct regions and quickly viewing the comparison)
     // example showing how to loop through results tiles
-    this.props.results.map(annotation_result => {
-      annotation_result.map((tile_overlay, id) => {
-        console.log(`results has tile ${id}`);
-      });
-    });
+    // THIS IS NOT WORKING - should return an array like so:
+    // [ {x: annotation_name, y:number_of_cells},
+    //   {x: annotation_name2, y:number_of_cells2},
+    //   ...]
+    let number_of_cells_in_each_region = 
+      Array.from(this.props.results.map((annotation_result, annotation_name) => {
+        let cell_count = annotation_result.reduce((sum, tile_cell_data, id) => {
+          return sum + tile_cell_data.centroids_x.size;
+        }, 0);
+        return {x: annotation_name, y: cell_count};
+      }));
+    if (number_of_cells_in_each_region.length == 0) {
+      number_of_cells_in_each_region = [{x: "none", y:1}];
+    }
+
     return (
       <WindowPortal>
       <div id="Analytics">
@@ -26,7 +41,15 @@ class Analytics extends React.Component {
       <Divider/>
       <div className="row">
         <div className="column">
-          <VictoryBar/>
+          <VictoryChart
+            theme={VictoryTheme.material}
+            domainPadding={10}
+          >
+            <VictoryBar
+              style={{ data: { fill: "#c43a31" } }}
+              data={number_of_cells_in_each_region}
+            />
+          </VictoryChart>
           <VictoryBar/>
         </div>
         <div className="column">
