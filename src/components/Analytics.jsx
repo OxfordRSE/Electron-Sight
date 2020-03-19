129f8b40f38b09b53817e14e5f7597b85e2e2ba4
Martin Robinson <martinjrobins@gmail.com>
2020-03-19 17:27:27 +0000

    #32 add nearest neighbour distance plot

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -4,8 +4,8 @@ import {
   Divider,
 } from "@blueprintjs/core";
 import {
-  VictoryBar, 
-  VictoryChart, 
+  VictoryBar,
+  VictoryChart,
   VictoryTheme,
   VictoryAxis,
 } from 'victory';
@@ -15,10 +15,72 @@ function determinantPointPair(p1, p2) {
   return p1.x * p2.y - p1.y * p2.x;
 }
 
+function distancePointPair(p1, p2) {
+  return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+}
+
 function count_cells_in_annotation(annotation_result) {
   return annotation_result.reduce((sum, tile_cell_data, id) => {
     return sum + tile_cell_data.centroids_x.size;
   }, 0);
+}
+
+function nearest_neighbour_distances_tile(tile_cell_data) {
+  let n = tile_cell_data.centroids_x.size;
+  let distances = []
+  for (let i = 0; i < n; i++) {
+    let p1 = {
+      x: tile_cell_data.centroids_x.get(i),
+      y: tile_cell_data.centroids_y.get(i)
+    }
+    let min_distance = Number.MAX_VALUE;
+    for (let j = 0; j < n; j++) {
+      if (i != j) {
+        let p2 = {
+          x: tile_cell_data.centroids_x.get(j),
+          y: tile_cell_data.centroids_y.get(j)
+        }
+        let distance = distancePointPair(p1, p2);
+        if (distance < min_distance) {
+          min_distance = distance;
+        }
+      }
+    }
+    distances.push(min_distance);
+  }
+  return distances;
+}
+
+function nearest_neighbour_distances(annotation_result) {
+  return annotation_result.reduce((sum, tile_cell_data, id) => {
+    return sum.concat(nearest_neighbour_distances_tile(tile_cell_data));
+  }, []);
+}
+
+function histogram(data) {
+  let nBins = 10.0;
+  let min = Number.MAX_VALUE;
+  let max = 0.0;
+  let n = data.length;
+  for (let i = 0; i < n; i++) {
+    if (data[i] < min) {
+      min = data[i];
+    }
+    if (data[i] > max) {
+      max = data[i] + 10*Number.EPSILON;
+    }
+  }
+  let bin_width = (max - min) / nBins;
+  let bins = new Array(nBins);
+  for (let i = 0; i < nBins; i++) {
+    bins[i] = min + (i + 0.5) * bin_width;
+  }
+  let values = new Array(nBins).fill(0);
+  for (let i = 0; i < n; i++) {
+    let bin_index = Math.floor((data[i] - min) / bin_width);
+    values[bin_index]++;
+  }
+  return {bins, values};
 }
 
 function polygon_area(polygon) {
@@ -26,6 +88,44 @@ function polygon_area(polygon) {
   return vertex_pairs.reduce((sum, vertex_pair) => {
     return sum + determinantPointPair(vertex_pair[0], vertex_pair[1]);
   }, 0);
+}
+
+class NearestNeighbourDistances extends React.Component {
+  render() {
+    let nn_distances =
+      this.props.results.map((annotation_result, annotation_name) => {
+        let distances = nearest_neighbour_distances(annotation_result);
+        const {bins, values} = histogram(distances);
+        return bins.map((bin, i) => {
+          return {x: bin, y: values[i]};
+        });
+      });
+    
+
+    return (
+      <VictoryChart
+        theme={VictoryTheme.material}
+        domainPadding={10}
+      >
+        <VictoryAxis label="Annotation" />
+        <VictoryAxis dependentAxis label="Count" />
+        {
+          nn_distances.toArray().map((x) => {
+            let name = x[0];
+            let data = x[1];
+          return (
+            <VictoryBar
+              key={name}
+              interpolation="step"
+              style={{data: {fill: "#c43a31"}}}
+              data={data}
+            />
+          );
+          })
+        }
+      </VictoryChart>
+    );
+  }
 }
 
 
@@ -45,8 +145,8 @@ class CellCount extends React.Component {
         theme={VictoryTheme.material}
         domainPadding={10}
       >
-        <VictoryAxis label="Annotation"/>
-        <VictoryAxis dependentAxis label="Cell Count"/>
+        <VictoryAxis label="Annotation" />
+        <VictoryAxis dependentAxis label="Cell Count" />
         <VictoryBar
           style={{data: {fill: "#c43a31"}}}
           data={number_of_cells_in_each_region}
@@ -72,8 +172,8 @@ class CellDensity extends React.Component {
         theme={VictoryTheme.material}
         domainPadding={10}
       >
-        <VictoryAxis label="Annotation"/>
-        <VictoryAxis dependentAxis label="Cell Density"/>
+        <VictoryAxis label="Annotation" />
+        <VictoryAxis dependentAxis label="Cell Density" />
         <VictoryBar
           style={{data: {fill: "#c43a31"}}}
           data={density_of_cells_in_each_region}
@@ -82,7 +182,7 @@ class CellDensity extends React.Component {
     );
   }
 }
- 
+
 
 
 class Analytics extends React.Component {
@@ -97,11 +197,6 @@ class Analytics extends React.Component {
     //Distributions of distance between NN cells
     //Distributions of distance between randomly chosen point and nearest cell (Spherical Contact Distance - SCD)
     //Differences between these things in different annotated regions (e.g., annotating 2 distinct regions and quickly viewing the comparison)
-    // example showing how to loop through results tiles
-    // THIS IS NOT WORKING - should return an array like so:
-    // [ {x: annotation_name, y:number_of_cells},
-    //   {x: annotation_name2, y:number_of_cells2},
-    //   ...]
 
     return (
       <WindowPortal>
@@ -110,7 +205,7 @@ class Analytics extends React.Component {
           <Divider />
           <div className="row">
             <div className="column">
-              <CellCount 
+              <CellCount
                 results={this.props.results}
                 annotations={this.props.annotations}
               />
@@ -120,7 +215,10 @@ class Analytics extends React.Component {
               />
             </div>
             <div className="column">
-              <VictoryBar />
+              <NearestNeighbourDistances
+                results={this.props.results}
+                annotations={this.props.annotations}
+              />
               <VictoryBar />
             </div>
           </div>
